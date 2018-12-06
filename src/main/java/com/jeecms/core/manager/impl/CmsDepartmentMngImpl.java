84d76ed9e55c654f0e0cd825c4bf9b79568987df
tamsiuloong <327395128@qq.com>
2018-12-06 16:29:59 +0800

    州市部门工作量统计-加入缓存功能 10秒

--- src/main/java/com/jeecms/core/manager/impl/CmsDepartmentMngImpl.java
+++ src/main/java/com/jeecms/core/manager/impl/CmsDepartmentMngImpl.java
@@ -9,7 +9,10 @@ import com.jeecms.common.page.Pagination;
 import com.jeecms.core.dao.CmsDepartmentDao;
 import com.jeecms.core.entity.CmsDepartment;
 import com.jeecms.core.manager.CmsDepartmentMng;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -141,28 +144,45 @@ public class CmsDepartmentMngImpl implements CmsDepartmentMng {
 
 	@Override
 	public List workload(Integer pid, Integer pageSize) {
-		List<Object[]> result = new ArrayList();
-		Map<String, Integer> map = new HashMap<>();
-		List<CmsDepartment> departmentList = dao.findByParentId(pid, pageSize);
-		departmentList.forEach(depart->{
-			Integer nums = dao.getWorkloadById(depart.getId());
-			map.put(depart.getName(), nums==null?0:nums);
-		});
-		List<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
-		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-			// 降序排序
-			@Override
-			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-				return -o1.getValue().compareTo(o2.getValue());
-			}
-		});
+		List<Object[]> result = null;
+		Element ele = cache.get("workload");
 
-		list.forEach(e->{
-			Object[] o = new Object[2];
-			o[0]=e.getKey();
-			o[1]=e.getValue();
-			result.add(o);
-		});
+		if(ele!=null)
+		{
+			result = (List<Object[]>) ele.getObjectValue();
+		}
+
+
+		if(result == null)
+		{
+			List<Object[]> r = new ArrayList<>(pageSize);
+
+			Map<String, Integer> map = new HashMap<>();
+			List<CmsDepartment> departmentList = dao.findByParentId(pid, pageSize);
+			departmentList.forEach(depart->{
+				Integer nums = dao.getWorkloadById(depart.getId());
+				map.put(depart.getName(), nums==null?0:nums);
+			});
+			List<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(map.entrySet());
+			Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+				// 降序排序
+				@Override
+				public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+					return -o1.getValue().compareTo(o2.getValue());
+				}
+			});
+
+			list.forEach(e->{
+				Object[] o = new Object[2];
+				o[0]=e.getKey();
+				o[1]=e.getValue();
+				r.add(o);
+			});
+
+			result = r;
+			cache.put(new Element("workload",result));
+		}
+
 		return result;
 	}
 
@@ -172,6 +192,10 @@ public class CmsDepartmentMngImpl implements CmsDepartmentMng {
 	private ChannelMng channelMng;
 	@Autowired
 	private CmsGuestbookCtgMng guestBookCtgMng;
+
+	@Autowired
+	@Qualifier("workload")
+	private Ehcache cache;
 
 	@Autowired
 	public void setDao(CmsDepartmentDao dao) {
